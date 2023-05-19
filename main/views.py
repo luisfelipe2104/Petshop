3813a4e8fa5f10476306f78ce058cb05bd3d8550
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-05-19 16:13:03 -0300

    fixing styles bugs

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+#  python3 manage.py collectstatic
+
 # Create your views here.
 def Home(request):
     return render(request, 'home.html')
--- petshop/settings.py
+++ petshop/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+
+    'main.apps.MainConfig',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
