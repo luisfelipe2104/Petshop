9493e83d7c2dfe4e0844845cdebf8d3074ebfd15
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-05-23 16:34:26 -0300

    consuming backend

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
+    path('index', views.Index, name='index'),
     path('login', views.Login, name='login'),
+    path('logout', views.Logout, name='logout'),
     path('register', views.Register, name='register'),
 ]
--- petshop/settings.py
+++ petshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,15 @@ WSGI_APPLICATION = 'petshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DEBUG = config('DEBUG', cast=bool)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT', cast=int),
     }
 }
 
